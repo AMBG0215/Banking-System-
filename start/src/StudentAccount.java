4c46ca29fbf4d316e258fccaac843ef7865bcc03
Anna Mikaela Gavieres <Ambgavieres@mymail.mapua.edu.ph>
2026-09-05 02:30:57 +0800

    student account updated
    added account type at student account

--- start/src/StudentAccount.java
+++ start/src/StudentAccount.java
@@ -1,3 +1,16 @@
-public class StudentAccount {
+class StudentAccount extends BankAccount {
+
+        public StudentAccount(String number, String name, float balance, boolean isActive){
+            super(number, name, balance, isActive);
+        }
+
+        @Override
+         public void showAccountType(){
+            System.out.println("Account Type: " + "Student Account");
+        }
+
+
+
+
 
 }
