2679462fc9627bbafa7004b87d1d976af1c3b529
Anna Mikaela Gavieres <Ambgavieres@mymail.mapua.edu.ph>
2026-09-04 20:08:24 +0800

    testing

--- start/src/SavingsAccount.java
+++ start/src/SavingsAccount.java
@@ -8,6 +8,7 @@ class SavingsAccount extends BankAccount {
      void withdraw(int amount){
         if(getBalance() < 100){
             // finish the logic, savings account should be with a minimum balance of P100, if the balance is 500, and withdrawed 450, it shouldn't be allowed because the minimum balance should be 100.
+            // try comment
         }
     }
 
